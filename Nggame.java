1a01268562e1781ed3ad7e646de9de2e25e945ff
pinkesh-kumar <142135331+pinkesh-kumar@users.noreply.github.com>
2023-08-13 11:27:20 +0530

    Add files via upload

---
+++ Nggame.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Nggame {
+    public static void main(String[] args)
+    {
+    Scanner reader =new Scanner(System.in);
+    String play ="yes";
+    while(play.equals("yes"));
+    {
+        Random rand = new Random();
+        int randNum = rand.nextInt(100);
+        int guess =-1;
+        int tries = 0;
+        while(guess !=randNum)
+        {
+            System.out.print("Guess a number between 1 and 100: ");
+            guess = reader.nextInt();
+            tries++;
+            if(guess == randNum)
+            {
+                System.out.println("Awesome! You guessed the number!");
+                System.out.println("It only took you " +tries +"guesses!");
+                System.out.print("Would you like to play again? Yes or No: ");
+                play = reader.next().toLowerCase();
+            }
+            else if(guess >randNum)
+            {
+                System.out.print("Your guess is too high , try again. ");
+            }
+            else
+            {
+                System.out.print("Your guess is  too Low , try again. ");
+            }
+        }
+        
+    }
+      reader.close();  
+    }
+    
+}
